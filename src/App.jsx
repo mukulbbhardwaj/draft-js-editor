c84a5423eed391d5aa8ece95d80b97599cd7e057
mukulbbhardwaj <themukulbhardwaj@example.com>
2024-01-24 12:28:50 +0530

    added instructions to use

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useState, useEffect } from "react";
 import { EditorState, RichUtils, convertToRaw, convertFromRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
@@ -28,8 +29,10 @@ function App() {
       currentSelection.getStartKey()
     );
     const currentText = currentBlock.getText();
+    console.log(chars);
     // '#' => Heading
     if (chars === "#" && currentText.trim() === "") {
+      console.log("heading");
       const newEditorState = RichUtils.toggleBlockType(
         editorState,
         "header-one"
@@ -39,12 +42,13 @@ function App() {
     }
     // '*' => Bold
     if (chars === "*" && currentText.trim() === "") {
+      console.log("bold");
       const newEditorState = RichUtils.toggleInlineStyle(editorState, "BOLD");
       setEditorState(newEditorState);
       return "handled";
     }
-    // '^' => Underline
-    if (chars === "^" && currentText.trim() === "") {
+    // '!' => Underline
+    if (chars === "!" && currentText.trim() === "") {
       const newEditorState = RichUtils.toggleInlineStyle(
         editorState,
         "UNDERLINE"
@@ -52,8 +56,9 @@ function App() {
       setEditorState(newEditorState);
       return "handled";
     }
-    // '!'=> To give Red Color
-    if (chars === "!" && currentText.trim() === "") {
+    // '@'=> To give Red Color
+    if (chars === "@" && currentText.trim() === "") {
+      console.log("redline");
       const newEditorState = RichUtils.toggleInlineStyle(
         editorState,
         "RED_LINE"
@@ -72,7 +77,21 @@ function App() {
     <div className="App">
       <div className="header">
         <header className="header-text">Demo editor by Mukul Bhardwaj</header>
-        <button className="save-button" onClick={saveToLocalStorage}>Save</button>
+        <button className="save-button" onClick={saveToLocalStorage}>
+          Save
+        </button>
+      </div>
+      <div className="instructions-box">
+        <h4>How to use :-</h4>
+        <p>
+          <ol>
+            <li>Use '#' as the first character to get heading.</li>
+            <li>Use '*' as the first character to get bold text.</li>
+            <li>Use '!' as the first character to get underline.</li>
+            <li>Use '@' as the first character to get red line.</li>
+            <li>Hit 'Save' button to save the current text to storage.</li>
+          </ol>
+        </p>
       </div>
       <Editor
         editorState={editorState}
@@ -83,6 +102,7 @@ function App() {
         toolbarClassName="toolbar-class"
         handleBeforeInput={handleBeforeInput}
         customStyleMap={styleMap}
+        placeholder="Start typing here ..."
       />
     </div>
   );
